b47f8aa2ca7170157329fd2a3e90a2731e8a38cd
abimanyu2515 <ctsssabimanyu.s@gmail.com>
2025-11-01 03:19:14 +0530

    implement hero section

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,13 +3,13 @@ import logo from '/logo.svg';
 
 const NavBar = () => {
   return (
-    <header>
-        <nav className='flex justify-around items-center'>
-            <img src={logo} alt="" />
+    <header className='w-screen fixed top-0 left-0 z-50 flex-center bg-black min-h-[7vh]'>
+        <nav className='flex justify-around items-center container mx-auto flex-between px-5 2xl:px-0'>
+            <img src={logo} className='cursor-pointer hover:-translate-y-0.5 transition-all duration-300 ease-in-out' alt="" />
 
-            <ul>
+            <ul className='flex-center gap-8 bg-transparent border-none outline-none cursor-pointer'>
                 {navLinks.map(({label}) => (
-                    <li key={label}>
+                    <li key={label} className='hidden md:block text-white opacity-80 font-regular text-sm cursor-pointer hover:opacity-100 transition-all duration-300 ease-in-out'>
                         <a href={label}>{label}</a>
                     </li>
                 ))}
