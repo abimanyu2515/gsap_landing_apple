a3d4f6faa83394cbf013af813ab032ecc7221ff4
abimanyu2515 <ctsssabimanyu.s@gmail.com>
2025-11-09 02:46:02 +0530

    Footer section complete

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,29 @@
+import { footerLinks } from "../constants"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="info">
+        <p>More ways to buy: Find a near by Apple Store</p>
+        <img src="/logo.svg" alt="logo" />
+      </div>
+      
+      <hr />
+
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li>
+              <a href={link}>
+                {label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
